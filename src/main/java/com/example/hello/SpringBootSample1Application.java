fb67e0fddad4cf060c3cf5c32d7cc7d453fabb3c
なり <nari1226@icloud.com>
2022-03-19 11:35:18 +0900

    nariduka

---
+++ src/main/java/com/example/hello/SpringBootSample1Application.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSample1Application.class, args);
+	}
+
+}
